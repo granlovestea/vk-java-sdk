2c665e281a197dd67ea6f0f06ef5db07fe927189
Artemiy Prikupets <artemprikupets@gmail.com>
2021-02-18 04:38:30 +0300

    Revert "fix(Message): now it can read body of message"
    This reverts commit 0b4f2b5a9813e5cc3aa746b9ec40beca2973d5fb.

--- sdk/src/main/java/com/vk/api/sdk/objects/messages/Message.java
+++ sdk/src/main/java/com/vk/api/sdk/objects/messages/Message.java
@@ -124,7 +124,7 @@ public class Message implements Validable {
     /**
      * Message text
      */
-    @SerializedName("body")
+    @SerializedName("text")
     @Required
     private String text;
 
